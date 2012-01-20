2e8a66d17c79cacc4577b78a947a192624cdb09d
Jaroslaw Nogala <jarek.nog@gmail.com>
2012-01-20 20:33:22 +0100

    some code improvment with Sigar library

--- PC_configuration/src/computer/Computer.java
+++ PC_configuration/src/computer/Computer.java
@@ -2,12 +2,10 @@ package computer;
 
 
 import computer.elevation.Elevation;
-import computer.services.Net;
-import computer.services.Sc;
-import computer.services.ServiceObj;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
@@ -16,55 +14,159 @@ import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SpringLayout;
-import javax.swing.border.EmptyBorder;
+
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SigarProxy;
+import org.hyperic.sigar.SigarProxyCache;
+import org.hyperic.sigar.win32.Service;
+import org.hyperic.sigar.win32.Win32Exception;
 
 public class Computer {
 
     public static JTextArea mJTextArea = new JTextArea();
     public static JList mJList;
     
-    public static void main(String[] args) {
-        makeDialog();
+    private static Sigar sigar = new Sigar();
+    private static SigarProxy sigarProxy = null;
+    
+    public static void main(String[] args){
         
-        Sc mSc = new Sc();
-        String str = "";
-        for(ServiceObj i : mSc.getServicesList()){
-            str += i.DISPLAY_NAME + "  :   " + i.SERVICE_NAME + "\n";
+        if(true){//args.length >0 && args[0].equals("elevated")){//
+            //do program
+            makeDialog();
+            addText("1");
+            sigarProxy  = SigarProxyCache.newInstance(new Sigar());
+            addText("2");
+            List ServicesNames = new ArrayList();
+            try
+            {   
+                ServicesNames = Service.getServiceNames(sigar,null);
+            }catch (Exception e){ 
+                    addText("Exception: " + e.toString());
+            }
+            addText("3");
+            for(Object i : ServicesNames){
+//                addText("3.5");
+                String data = i.toString() + " : ";
+//                addText("3.7");
+//                addText("Service name: " + data);
+                try
+                {  
+                    Service mService = new Service(i.toString());
+//                    addText("4");
+                    if(mService.getStatus() == Service.SERVICE_RUNNING){
+                        try {
+                            addText(data + sigarProxy.getProcCpu(sigarProxy.getServicePid(i.toString())).toString());
+                        } catch (SigarException ex) {
+                            addText("Exception: " + ex.toString());
+                        }
+//                        addText("5");
+                        for(String j : mService.getConfig().getDependencies()){
+                            addText("- " + j);
+                        }
+                    }
+                }catch (Win32Exception e){ 
+                    addText("Exception: " + e.toString());
+                }
+            }
+                //routing table
+//                  for(NetRoute i :sigarProxy.getNetRouteList()){
+//                      addText(i.toString());
+//                  }
+                  //Usage of processor
+//                long id = 776;
+//                
+//                ProcCpu cpu = sigarProxy.getProcCpu(id);
+//                addText("cpu=" + cpu.toString());s
+//                while (true)
+//                {
+//                    cpu = sigarProxy.getProcCpu(id);
+//                    addText("cpu=" + cpu.toString());
+//
+//                    try {
+//                    Thread.sleep(2000);no 
+//                    } 
+//                    catch (InterruptedException ex) {}
+//                }
         }
-        addText(str);
-//        Vector names = mSc.getListOfServices();
-//
-//        String Effect = "";
-//        Computer.addText("Run matafaka");
-//        for(int i = 0; i < names.size(); i++){
-//            Effect += Net.getInfoAboutService(9,(String) names.elementAt(i)) + '\n';
-//        }
-//        addText(Effect);
-//        if(args.length >0 && args[0].equals("elevated")){
-//            //do program
-//            Net netObj = new Net();
-//            Vector names = netObj.getNetStartList();
-//
-//            String Effect = "";
-//            
-//            Computer.addText("Run matafaka");
-//            for(int i = 0; i < names.size(); i++){
-//                Effect += Net.getInfoAboutService(9,(String) names.elementAt(i)) + '\n';
-//            }
-//            addText(Effect);
-//        }
-//        else{//do elevation
-//            Elevation.getElevation();
-//        }
+        else{//do elevation
+            Elevation.getElevation();
+        }
         
         
+        
+        
+        /*
+        
+        
+//        ProcessorInfo info = new ProcessorInfo();
+//        Long first = info.getProcessCPUTime(624);
+//        try {
+//            Thread.sleep(2000);
+//            } catch (InterruptedException ex) {
+//        }
+//        
+//        Long second = info.getProcessCPUTime(624);
+        
+        
+        Sigar sigar = new Sigar();
+
+        
+  
+        
+        
+        ProcCpu mProcCpu = new MultiProcCpu();
+       
+       
+       
+        mProcCpu.getPercent();
+        try {
+            ProcState procState = sigar.getProcState("624");
+//            ProcCpu procCpu = sigar.getProcCpu("624"); //take too much time
+            ProcStat mStat =  sigar.getProcStat();//statystyki procesora
+//            ProcTime mProcTime = sigar.getProcTime("623");
+            
+            //ProcCpu procCpu = sigar.getProcCpu("624");
+            
+            addText(procState.getName() + ": " +
+                           procState.getState());
+            
+//            addText("ProcCpu: " + procCpu.toString());
+            addText("ProcStat: " + mStat.getRunning());
+//            addText("ProcTime: " + sigar.getProcFd("623").getTotal());//tez sie zatrzymuje
+            addText("ProcTime: " + sigar.getProcCred("623").getName());//tez sie zatrzymuje
+            
+            for(CpuPerc i : sigar.getCpuPercList())
+                addText(i.toString());
+                 
+     //            JavaSysMon monitor =   new JavaSysMon();
+     //            ProcessInfo[] processtab = monitor.processTable();
+     //            for(ProcessInfo i : processtab){
+     //                if(i.getName().toLowerCase().contains("unknow"))//onlu pid's from unknown processes
+     //                    addText(String.valueOf(i.getPid()));
+     //                
+     //            }
+     //            
+     //            List list = monitor.processTree().children();
+     //            
+     //            for(int i =0; i<list.size() ;i++){
+     //                addText(list.get(i).toString());
+     //            }
+
+     
+        } catch (SigarException ex) {
+        }
+        
+        
+        */
     }
     
     
    public static void makeDialog(){
-      JFrame frame = new JFrame("Show Message Dialog");
+        JFrame frame = new JFrame("Show Message Dialog");
       
-      SpringLayout mSpringLayout = new SpringLayout();
+        SpringLayout mSpringLayout = new SpringLayout();
         frame.setLayout(mSpringLayout);
         frame.setSize(600, 600);
         
@@ -76,70 +178,21 @@ public class Computer {
         
         JScrollPane mJScrollPane = new JScrollPane(mJList);
         mJScrollPane.setPreferredSize(  new Dimension(550, 500));
-        frame.add(mJScrollPane);
-        //mSpringLayout.putConstraint(SpringLayout.EAST, mTextField, 20,SpringLayout.EAST, mJCheckBox);
-//        mSpringLayout.putConstraint(SpringLayout.NORTH, mJScrollPane, 30,SpringLayout.NORTH, mJButton);
-        
-      
-//      frame.setadd(mJTextArea);
-      
-      frame.setVisible(true);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(mJScrollPane);      
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    }
    
    public static void addText(String newText){
-       String text = mJTextArea.getText() + "\r\n" + newText;
-       mJTextArea.setText(text);
-      DefaultListModel mListModel = (DefaultListModel) mJList.getModel();
-      if(!newText.equals(""))
-        mListModel.addElement(newText);
+        String text = mJTextArea.getText() + "\r\n" + newText;
+        mJTextArea.setText(text);
+        DefaultListModel mListModel = (DefaultListModel) mJList.getModel();
+        if(!newText.equals(""))
+            mListModel.addElement(newText);
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException ex) {
+        }
    }
-
-    
-            
-    //        SystemInfo si = new SystemInfo();
-    //        OperatingSytem os = si.getOperatingSystem();
-    //        System.out.println(os);
-    //        HardwareAbstractionLayer hal = si.getHardware();
-    //        System.out.println(hal.getProcessors().length + " CPU(s):");
-    //        for (Processor cpu : hal.getProcessors()) {
-    //            System.out.println(" " + cpu);
-    //        }
-    //        System.out.println("Memory: "
-    //                + FormatUtil.formatBytes(hal.getMemory().getAvailable()) + "/"
-    //                + FormatUtil.formatBytes(hal.getMemory().getTotal()));
-            
-            
-            
-            
-            
-    //    /* Total number of processors or cores available to the JVM */
-    //    System.out.println("Available processors (cores): " + 
-    //        Runtime.getRuntime().availableProcessors());
-    //
-    //    /* Total amount of free memory available to the JVM */
-    //    System.out.println("Free memory (bytes): " + 
-    //        Runtime.getRuntime().freeMemory());
-    //
-    //    /* This will return Long.MAX_VALUE if there is no preset limit */
-    //    long maxMemory = Runtime.getRuntime().maxMemory();
-    //    /* Maximum amount of memory the JVM will attempt to use */
-    //    System.out.println("Maximum memory (bytes): " + 
-    //        (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
-    //
-    //    /* Total memory currently in use by the JVM */
-    //    System.out.println("Total memory (bytes): " + 
-    //        Runtime.getRuntime().totalMemory());
-    //
-    //    /* Get a list of all filesystem roots on this system */
-    //    File[] roots = File.listRoots();
-    //
-    //    /* For each filesystem root, print some info */
-    //    for (File root : roots) {
-    //      System.out.println("File system root: " + root.getAbsolutePath());
-    //      System.out.println("Total space (bytes): " + root.getTotalSpace());
-    //      System.out.println("Free space (bytes): " + root.getFreeSpace());
-    //      System.out.println("Usable space (bytes): " + root.getUsableSpace());
-    //    }
     
 }
